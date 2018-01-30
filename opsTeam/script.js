554bac7aef082e2e1b5234f83b67504352c29984
samMw <35973649+samMw@users.noreply.github.com>
2018-01-30 20:52:55 +0000

    Delete script.js

--- opsTeam/script.js
+++
@@ -1,5 +0,0 @@
-function signImage(e) {
-    $('.modal-title').text("Sign Position No." + e);
-    document.getElementById('modalImage').src="Photos\\" + e + ".jpg"
-    $('#exampleModal').show()
-}
