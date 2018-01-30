06888a7ae66991fc18260053a1cb9b8c8347c643
samMw <35973649+samMw@users.noreply.github.com>
2018-01-30 20:35:38 +0000

    Update script.js

--- opsTeam/script.js
+++ opsTeam/script.js
@@ -1,24 +1,5 @@
-$('#exampleModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    var photo = button.data('whatever') // Extract info from data-* attributes
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this)
-    modal.find('.modal-title').text('Sign Position No.' + photo)
-    modal.find('.modal-body input').val(photo)
-
-    $('.modal-body').text("The example script ran, the btn was " + photo);
+function signImage(e) {
+    $('.modal-title').text("Sign Position No." + e);
+    document.getElementById('modalImage').src="Photos\\" + e + ".jpg"
     $('#exampleModal').show()
-})
-
-function test(event) {
-
-    var image = $(this).attr('src');
-        $('#exampleModal').on('show.bs.modal', function () {
-            $(".img-fluid").attr("src", image);
-        });
-
-    //alert(event)
-    //$('.modal-body').text("The script ran, the ID was " + event);
-    //$('#exampleModal').show()
 }
